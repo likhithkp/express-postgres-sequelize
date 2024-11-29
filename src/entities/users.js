cd0ed4e8bcfdc5dde5f5f9e21184d346f6441749
likhithkp <likhithkpdev@gmail.com>
2024-11-29 09:19:49 +0530

    SOC

---
+++ src/entities/users.js
@@ -0,0 +1,30 @@
+const User = require("../../db/models/users");
+
+class Services {
+  constructor() {
+    this.entity = 'users';
+  }
+
+ async createUser({}){
+
+ } 
+ 
+ async getAllUsers({}){
+
+ } 
+ 
+ async getUser({}){
+
+ } 
+ 
+ async updateUser({}){
+
+ } 
+ 
+ async deleteUser({}){
+
+ }
+
+}
+
+module.exports = Services;
